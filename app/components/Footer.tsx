7f1276d081b6cfd2859949593762690a8830a314
RazielGelo <angeloguerra1986x@gmail.com>
2025-04-03 13:07:48 -0700

    Modified footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -37,6 +37,7 @@ const Footer = () => {
 				</motion.h2>
 
 				<p className='text-sm text-gray-400'>&copy; {currentYear} NT Enterprise. All rights reserved.</p>
+				<p className='text-xs text-gray-400 mt-1'>Built & Maintained by <strong>Angelo Guerra</strong></p>
 			</div>
 		</footer>
 	);
